ba442072653e3269ca44fc2d04377c3430f90554
chandraprakash <84634235+chandru-master@users.noreply.github.com>
2022-10-23 22:40:41 +0530

    Create question3.py

---
+++ best_binary_classification/question3.py
@@ -0,0 +1,4 @@
+Download an image dataset of your choice for binary class classification. Perform the data 
+augmentation techniques like flipping, rotation and transformation. Apply at least two object 
+classification techniques both on the augmented as well as on the original dataset. Display the 
+performance of the Algorithms
