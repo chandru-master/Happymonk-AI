b430790b39acb83d5fe5f82df31d6778f206fa8e
chandraprakash <84634235+chandru-master@users.noreply.github.com>
2022-10-24 09:57:30 +0530

    Create question4.py
    answer

---
+++ collection_regional_car_plate/question4.py
@@ -0,0 +1,4 @@
+Collect images of vehicles with license plates written in Indian regional languages (eg. Hindi, 
+Kannada, Tamil, Telugu, Bengali, etc.). Apply Image augmentation techniques on the collected 
+images. Maintain separate folders for different language license plates. You may limit to 800 
+images in the dataset including the augmented images.
